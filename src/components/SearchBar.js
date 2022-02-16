ce33f5fb260528793e649660ec634fb7a0e94234
Barrettnj0 <63025815+Barrettnj0@users.noreply.github.com>
2022-02-16 15:00:36 -0600

    Added state dropdown and functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,21 +4,32 @@ import { getWeather } from '../api';
 const SearchBar = (props) => {
 
     const [city, setCity] = useState("");
+    const [state, setState] = useState("");
     let stateCodes = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']
 
-    const handleChange = (e) => {
+    const handleCityChange = (e) => {
         setCity(e.target.value);
+    }
+
+    const handleStateChange= (e) => {
+        setState(e.target.value);
     }
 
     const handleKeyDown = (e) => {
         if (e.keyCode === 13) {
-            getWeather(city);
+            getWeather(city, state);
         }
     }
 
     return (
         <div>
-            <input onChange={handleChange} value={city} placeholder="Enter City" onKeyDown={handleKeyDown} />
+            <input onChange={handleCityChange} value={city} placeholder="Enter City" onKeyDown={handleKeyDown} />
+            
+            <select onChange={handleStateChange}>
+                {stateCodes.map(code =>
+                <option key={stateCodes.indexOf(code)} value={code}>{code}</option>
+                )}
+            </select>
         </div>
     )
 }
