8c4206ae491701e6eeb8bd2dc4eedddf93fc5ca5
Barrettnj0 <63025815+Barrettnj0@users.noreply.github.com>
2022-02-17 15:54:08 -0600

    add content to controller, model, repo, and service

--- server/weather-app/src/main/java/com/example/weatherapp/model/Users.java
+++ server/weather-app/src/main/java/com/example/weatherapp/model/Users.java
@@ -22,4 +22,35 @@ public class Users {
     @Column
     @NotNull
     private String password;
+
+    public Users() {}
+
+    public Users(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- server/weather-app/src/main/java/com/example/weatherapp/service/UsersService.java
+++ server/weather-app/src/main/java/com/example/weatherapp/service/UsersService.java
@@ -1,4 +1,17 @@
-package service;
+package com.example.weatherapp.service;
 
+import com.example.weatherapp.model.Users;
+import com.example.weatherapp.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UsersService {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    public void test(String username, String password) {
+        usersRepository.save(new Users(username, password));
+    }
 }
