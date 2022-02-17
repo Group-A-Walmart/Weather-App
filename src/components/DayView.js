16419d269b81f66bc4966d66a703269df7395d4c
Rachel Eckels <Rachel.Eckels@walmart.com>
2022-02-17 13:35:46 -0600

    add details

--- src/components/DayView.js
+++ src/components/DayView.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import clouds from '../images/clouds.jpg';
 import '../styles/DayView.css';
-import kelvinToFahrenheit from '../helpers';
-
+import {kelvinToFahrenheit, getWeatherPic} from '../helpers';
 
 const DayView = (props) => {
     const main = props.weatherData.main;
     const name = props.weatherData.name;
     const temp = main.temp;
-    console.log(temp);
+    const newTemp = Math.trunc(kelvinToFahrenheit(temp));
+    const weather = props.weatherData.weather[0].main;
+    let weatherDescription = props.weatherData.weather[0].description;
+    weatherDescription = weatherDescription.split(" ").map((word) => word.charAt(0).toUpperCase() + word.substring(1)).join(" ");
+    console.log(weather);
+    const imageUrl = getWeatherPic(weather);
+    const feelsLike = Math.trunc(kelvinToFahrenheit(props.weatherData.main.feels_like));
+    const humidity = props.weatherData.main.humidity;
+    const windSpeed = Math.round(props.weatherData.wind.speed);
+
+
 
     return (
         <div className="day-view">
-           <img src={clouds} alt="weather image"/>
-           <h1>72°</h1>
+           <img src={imageUrl} alt="weather image"/>
+           <h1>{newTemp}°</h1>
            <h2>{name}, {props.state}</h2>
+           <div className="details">
+                <h3>{weatherDescription}</h3>
+                <h3>Feels Like: {feelsLike}°</h3>
+                <h3>Wind: {windSpeed}mph</h3>
+                <h3>Humidity: {humidity}%</h3>
+           </div>
         </div>
     )
 }
