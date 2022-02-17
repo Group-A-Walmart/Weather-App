a157b1040d8c556bc7d5ff6af5cb57b9584baf4a
Barrettnj0 <63025815+Barrettnj0@users.noreply.github.com>
2022-02-17 10:54:22 -0600

    fix functionality of toggle

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ function App() {
   const [isWeeklyView, setIsWeeklyView] = useState(false);
   const [weatherData, setWeatherData] = useState({});
   const [weeklyView, setWeeklyView] = useState({});
-  const _toggleWeeklyView = () => setIsWeeklyView(!setWeeklyView);
+  const _toggleWeeklyView = () => setIsWeeklyView(!isWeeklyView);
 
 
   const handleCityChange = (e) => {
