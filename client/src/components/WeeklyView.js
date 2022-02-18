c162a00e7d8965cd4581781271cb7d75d011a692
Rachel Eckels <Rachel.Eckels@walmart.com>
2022-02-18 08:51:41 -0600

    Add stylings

--- client/src/components/WeeklyView.js
+++ client/src/components/WeeklyView.js
@@ -4,6 +4,10 @@ import "../styles/WeeklyView.css";
 const WeeklyView = (props) => {
     console.log(props.weeklyView)
     let { daily } = props.weeklyView;
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat","Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat","Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const date = new Date();
+    let currentDay;
+
     return (
         <div id="WeeklyView">
             {daily && daily.map((day, i) => {
@@ -14,8 +18,9 @@ const WeeklyView = (props) => {
                         <div className="daily-weather-cards">
                             <img src={imgURL} />
                             <div className="daily-weather">
-                                <p>Low: {Math.trunc(kelvinToFahrenheit(day.temp.min))}</p>
-                                <p>High: {Math.trunc(kelvinToFahrenheit(day.temp.max))}</p>
+                                <p id="day">{currentDay = days[date.getDay()+i]}</p>
+                                <p>High: {Math.trunc(kelvinToFahrenheit(day.temp.max))}°</p>
+                                <p>Low: {Math.trunc(kelvinToFahrenheit(day.temp.min))}°</p>
                             </div>
                         </div>
                     </div>
