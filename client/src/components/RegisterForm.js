136604212f06b84ee2ba582fd95c10e16346690d
Kris Norful <krisnorful@gmail.com>
2022-02-18 09:51:46 -0600

    register button navigates user to weather app

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -1,17 +1,19 @@
 import { useState } from "react"
 import { addUser } from "../api";
+import { useNavigate } from "react-router-dom";
 import "../styles/RegisterForm.css";
 
 const RegisterForm = (props) => {
 
+    const navigate = useNavigate();
     const [username, setUsername] = useState("");
-    const handleUsernameChange = ({target}) => {
+    const handleUsernameChange = ({ target }) => {
         let newUsername = target.value;
         setUsername(newUsername);
     }
 
     const [password, setPassword] = useState("");
-    const handlePasswordChange = ({target}) => {
+    const handlePasswordChange = ({ target }) => {
         let newPassword = target.value;
         setPassword(newPassword);
     }
@@ -25,12 +27,13 @@ const RegisterForm = (props) => {
         }
 
         addUser(newUser);
+        setTimeout(() => navigate("/weather-app"), 800);
     }
 
     return (
         <form id="RegisterForm" onSubmit={handleSubmit}>
             <h2>Register</h2>
-            <input value={username} placeholder="Username" onChange={handleUsernameChange}  />
+            <input value={username} placeholder="Username" onChange={handleUsernameChange} />
             <input value={password} placeholder="Password" onChange={handlePasswordChange} />
             <input type="submit" value="Register" />
         </form>
